9322e486fe44fbfeab83978cc74ae2f86519874e
panqifei <sysionpqf@sysion.com>
2016-06-29 14:15:49 +0800

    1.

---
+++ Demo/DinoKitDemo/DinoKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DinoKitDemo
+//
+//  Created by xjf on 16/6/29.
+//  Copyright © 2016年 Dino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo/DinoKitDemo/DinoKitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DinoKitDemo
+//
+//  Created by xjf on 16/6/29.
+//  Copyright © 2016年 Dino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

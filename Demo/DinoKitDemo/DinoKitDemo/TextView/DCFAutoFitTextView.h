c190a56442f714ae2ba4f7cb4f5de6150abd1ba7
panqifei <sysionpqf@sysion.com>
2016-07-04 17:23:13 +0800

    1.

---
+++ Demo/DinoKitDemo/DinoKitDemo/TextView/DCFAutoFitTextView.h
@@ -0,0 +1,13 @@
+//
+//  DCFAutoFitTextView.h
+//  ios_iphone_teacher
+//
+//  Created by xjf on 16/7/4.
+//  Copyright © 2016年 dqf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCFAutoFitTextView : UITextView
+
+@end
---
+++ Demo/DinoKitDemo/DinoKitDemo/TextView/DCFAutoFitTextView.m
@@ -0,0 +1,34 @@
+//
+//  DCFAutoFitTextView.m
+//  ios_iphone_teacher
+//
+//  Created by xjf on 16/7/4.
+//  Copyright © 2016年 dqf. All rights reserved.
+//
+
+#import "DCFAutoFitTextView.h"
+
+@interface DCFAutoFitTextView ()
+
+@property (nonatomic, strong) UITextView *textView;
+
+@end
+
+@implementation DCFAutoFitTextView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame])
+    {
+        
+        
+    }
+    return self;
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    
+}
+
+@end
